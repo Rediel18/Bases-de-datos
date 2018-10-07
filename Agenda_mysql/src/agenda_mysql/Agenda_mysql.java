eba184407c3414d89c1d4e36e28fda6054fa5900
Leider Correa <42642466+Rediel18@users.noreply.github.com>
2018-10-07 11:05:07 -0500

    Add files via upload

---
+++ Agenda_mysql/src/agenda_mysql/Agenda_mysql.java
@@ -0,0 +1,15 @@
+package agenda_mysql;
+
+import co.edu.udistrital.agenda.datos.DBConexion;
+import co.edu.udistrital.agenda.datos.DBContactos;
+import co.edu.udistrital.agenda.logica.Contacto;
+
+public class Agenda_mysql {
+
+    public static void main(String[] args) {
+        Tabla frame = new Tabla();
+        frame.pack();
+        frame.setVisible(true);
+        
+    }
+}
